a2264d62cfa00370989a2b8e71667cb87961eea0
mare <185629749+znlv@users.noreply.github.com>
2025-07-08 20:48:07 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,49 +1,192 @@
--- Credits To The Original Devs @xz, @goof
-getgenv().Config = {
-	Invite = "informant.wtf",
-	Version = "0.0",
-}
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("RZ Cheats | Universal", "DarkTheme")
 
-getgenv().luaguardvars = {
-	DiscordName = "92kx#0000",
-}
+local Tab1 = Window:NewTab("Configurations")
+local Section1 = Tab1:NewSection("Keybindings")
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Other/main/1"))()
+Section1:NewKeybind("Toggle Hub", "Minimalize", Enum.KeyCode.Insert, function()
+    Library:ToggleUI()
+end)
 
-library:init() -- Initalizes Library Do Not Delete This
+local Section5 = Tab1:NewSection("Credits")
 
-local Window = library.NewWindow({
-	title = "RZ :3 | The #1 Internal cheat",
-	size = UDim2.new(0, 525, 0, 650)
-})
 
-local tabs = {
-    Tab1 = Window:AddTab("Tab1"),
-	Settings = library:CreateSettingsTab(Window),
-}
 
--- 1 = Set Section Box To The Left
--- 2 = Set Section Box To The Right
+local function ActivateESP()
+    for i, v in pairs(game.Players:GetChildren()) do
+        local chr = v.Character
+        if chr:FindFirstChild('q') then
+            continue
+        else
+            local h = Instance.new('Highlight')
+            h.Name = 'q'
+            h.Parent = v.Character
+            h.FillTransparency = 0.3
+            h.OutlineColor = Color3.new(1, 0, 0)  -- Red outline color
+            h.FillColor = Color3.new(1, 1, 1)  -- White fill color
+            
+            local nameLabel = Instance.new("TextLabel")
+            nameLabel.Name = "NameLabel"
+            nameLabel.Parent = h
+            nameLabel.BackgroundTransparency = 1
+            nameLabel.Position = UDim2.new(0, 0, -0.2, 0)
+            nameLabel.Size = UDim2.new(1, 0, 0.2, 0)
+            nameLabel.Font = Enum.Font.SourceSans
+            nameLabel.Text = v.Name
+            nameLabel.TextColor3 = Color3.new(1, 1, 1)
+            nameLabel.TextScaled = true
+            nameLabel.TextSize = 14.0
+            nameLabel.TextWrapped = true
+        end
+    end
+end
 
-local sections = {
-	Section1 = tabs.Tab1:AddSection("Section1", 1),
-}
 
-sections.Section1:AddToggle({
-	enabled = true,
-	text = "Aimbot Toggle",
-	flag = "Toggle_1",
-	tooltip = "Tooltip1",
-	risky = true, -- turns text to red and sets label to risky
-	callback = function(lol)
-	    _G.AimbotEnabled = true
+local function DeactivateESP()
+	for i, v in pairs(game.Players:GetChildren()) do
+		local chr = v.Character
+		local highlight = chr:FindFirstChild('q')
+		if highlight then
+			highlight:Destroy()
+		end
 	end
-})
+end
 
 
-library:SendNotification("Notification", 5, Color3.new(255, 0, 0))
+local Tab = Window:NewTab("")
+local Section2 = Tab:NewSection("Universal")
 
---Window:SetOpen(true) -- Either Close Or Open Window
+_G.infinjump = true
+ 
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
+ 
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+k = k:lower()
+if k == "r" then
+if _G.infinjump == true then
+_G.infinjump = false
+else
+_G.infinjump = true
+end
+end
+end)
+end)
+
+Section2:NewToggle("Wallhack Toggle", "ToggleInfo", function(state)
+    if state then
+		ActivateESP()
+
+    else
+		DeactivateESP()
+    end
+end)
+
+
+
+-- Aimbot Lib
+local Camera = workspace.CurrentCamera
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local LocalPlayer = Players.LocalPlayer
+local Holding = false
+
+_G.AimbotEnabled = false
+_G.TeamCheck = false
+_G.AimPart = "Head"
+_G.Sensitivity = 0 
+
+local function GetClosestPlayer()
+	local MaximumDistance = math.huge
+	local Target = nil
+  
+  	coroutine.wrap(function()
+    		wait(20); MaximumDistance = math.huge 
+  	end)()
+
+	for _, v in next, Players:GetPlayers() do
+		if v.Name ~= LocalPlayer.Name then
+			if _G.TeamCheck == true then
+				if v.Team ~= LocalPlayer.Team then
+					if v.Character ~= nil then
+						if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+							if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+								local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+								local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+								
+								if VectorDistance < MaximumDistance then
+									Target = v
+                  							MaximumDistance = VectorDistance
+								end
+							end
+						end
+					end
+				end
+			else
+				if v.Character ~= nil then
+					if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+						if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+							local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+							local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+							
+							if VectorDistance < MaximumDistance then
+								Target = v
+               							MaximumDistance = VectorDistance
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+
+	return Target
+end
+
+UserInputService.InputBegan:Connect(function(Input)
+    if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+        Holding = true
+    end
+end)
+
+UserInputService.InputEnded:Connect(function(Input)
+    if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+        Holding = false
+    end
+end)
+
+RunService.RenderStepped:Connect(function()
+    if Holding == true and _G.AimbotEnabled == true then
+        TweenService:Create(Camera, TweenInfo.new(_G.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFrame.new(Camera.CFrame.Position, GetClosestPlayer().Character[_G.AimPart].Position)}):Play()
+    end
+end)
+
+
+Section2:NewToggle("Aimbot Toggle", "ToggleInfo", function(state)
+    if state then
+		_G.AimbotEnabled = true
+
+    else
+		_G.AimbotEnabled = false
+    end
+end)
+
+
+
 
 -- Aimbot Lib
 local Camera = workspace.CurrentCamera
